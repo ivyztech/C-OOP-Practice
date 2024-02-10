9e5543844842458dc4271f3b90ee9218a25a891a
ivyztech <108420656+ivyztech@users.noreply.github.com>
2024-02-10 20:21:09 +1100

    Add files via upload

---
+++ DoubleIntCastingPractice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice;
+
+    class Practice
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 17;
+            int count = 5;
+
+            int intAverage = sum / count;
+
+            Console.WriteLine("Integer Average:" + intAverage);
+
+            double doubleAverage = sum/count;
+            Console.WriteLine("Double Average:" + doubleAverage);
+
+            doubleAverage = (double)sum / count;
+            Console.WriteLine("Correct Double Average:" + doubleAverage);
+
+
+        }
+    }
+
