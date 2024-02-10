82cbba3681bbc1432381d4e5276ff74f1b763ce2
ivyztech <108420656+ivyztech@users.noreply.github.com>
2024-02-10 18:13:30 +1100

    Add files via upload

---
+++ 1.1P/CodeFragmentCorrections.cs
@@ -0,0 +1,70 @@
+﻿/* ---- CODE SNIPPET 1 ----
+
+    int height = 13;
+    if (height <= 12)
+        Console.WriteLine("Low bridge: ");
+        Console.WriteLine("proceed with caution");
+
+    - Though it is functionally correct, for the result to print out nothing 
+    since the height is higher than 12, we can add brackets.
+
+*/
+
+using System;
+
+public class Score 
+{
+    public static void Main(string[] args) 
+    {
+        int height = 13;
+        if (height <= 12)
+            { 
+                Console.WriteLine("Low bridge: ");
+                Console.WriteLine("proceed with caution.");
+            }
+    }
+}
+
+
+/* ---- CODE SNIPPET 2 ----
+
+    int sum = 21;
+    if ( sum != 20 )
+        Console.WriteLine ("You win ");
+    else
+        Console.WriteLine ("You lose ");
+        Console.WriteLine ("the prize.");
+
+    - This is correct.
+
+*/
+
+int sum = 21;
+if ( sum != 20 )
+    Console.WriteLine ("You win ");
+else
+    Console.WriteLine ("You lose ");
+    Console.WriteLine ("the prize.");
+
+
+/* ---- CODE SNIPPET 3 ----
+
+    int sum = 7;
+    if ( sum > 20 ) {
+        Console.WriteLine ("You win ");
+    } else {
+        Console.WriteLine ("You lose ");
+    }
+        Console.WriteLine ("the prize.");
+
+    - This is correct.
+
+*/
+
+int sum = 7;
+if ( sum > 20 ) {
+    Console.WriteLine ("You win ");
+} else {
+    Console.WriteLine ("You lose ");
+}
+    Console.WriteLine ("the prize.");
